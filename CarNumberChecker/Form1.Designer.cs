927a206f7c6582fe37809f932b16f51d5144125f
GEROMAX <tobita0112@gmail.com>
2016-12-22 11:27:52 +0900

    first time commit

---
+++ CarNumberChecker/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace CarNumberChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDataCreate = new System.Windows.Forms.Button();
+            this.btnDivideResult = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(384, 431);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnDataCreate
+            // 
+            this.btnDataCreate.Location = new System.Drawing.Point(12, 12);
+            this.btnDataCreate.Name = "btnDataCreate";
+            this.btnDataCreate.Size = new System.Drawing.Size(117, 23);
+            this.btnDataCreate.TabIndex = 1;
+            this.btnDataCreate.Text = "テストデータ生成";
+            this.btnDataCreate.UseVisualStyleBackColor = true;
+            this.btnDataCreate.Click += new System.EventHandler(this.btnDataCreate_Click);
+            // 
+            // btnDivideResult
+            // 
+            this.btnDivideResult.Location = new System.Drawing.Point(487, 12);
+            this.btnDivideResult.Name = "btnDivideResult";
+            this.btnDivideResult.Size = new System.Drawing.Size(75, 23);
+            this.btnDivideResult.TabIndex = 2;
+            this.btnDivideResult.Text = "仕分け";
+            this.btnDivideResult.UseVisualStyleBackColor = true;
+            this.btnDivideResult.Click += new System.EventHandler(this.btnDivideResult_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(487, 41);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 21;
+            this.dataGridView2.Size = new System.Drawing.Size(384, 431);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(877, 41);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 21;
+            this.dataGridView3.Size = new System.Drawing.Size(384, 431);
+            this.dataGridView3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1273, 484);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.btnDivideResult);
+            this.Controls.Add(this.btnDataCreate);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDataCreate;
+        private System.Windows.Forms.Button btnDivideResult;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+    }
+}
+
---
+++ CarNumberChecker/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarNumberChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public List<CarNumber> CarNumberList { get; set; }
+
+        private void btnDataCreate_Click(object sender, EventArgs e)
+        {
+            CarUsageCode[] carUsage = (CarUsageCode[])Enum.GetValues(typeof(CarUsageCode));
+
+            var randomizer = new Random(DateTime.Now.Second);
+            var randomAlphaNum = new RandomAlphaNumeric(randomizer);
+
+            this.CarNumberList = new List<CarNumber>();
+            for (int i = 1; i <= 100; i++)
+            {
+                string number = randomizer.Next(2, 10).ToString() + randomAlphaNum.ToString() + randomAlphaNum.ToString();
+                number = number.Substring(0, randomizer.Next(1, 4));
+                this.CarNumberList.Add(new CarNumber(carUsage[randomizer.Next(0, carUsage.Length)], number));
+            }
+
+            this.dataGridView1.DataSource = this.CarNumberList;
+        }
+
+        private void btnDivideResult_Click(object sender, EventArgs e)
+        {
+            if (null == this.CarNumberList)
+            {
+                return;
+            }
+
+            var lstOK = this.CarNumberList.FindAll(match => match.IsValidNumber);
+            lstOK.Sort((itemA, itemB) => itemA.CarUsage.Equals(itemB.CarUsage) ? itemA.ClassNumber.CompareTo(itemB.ClassNumber) : itemA.CarUsage.CompareTo(itemB.CarUsage));
+            this.dataGridView2.DataSource = lstOK;
+
+            var lstNG = this.CarNumberList.FindAll(match => !match.IsValidNumber);
+            lstNG.Sort((itemA, itemB) => itemA.CarUsage.Equals(itemB.CarUsage) ? itemA.ClassNumber.CompareTo(itemB.ClassNumber) : itemA.CarUsage.CompareTo(itemB.CarUsage));
+            this.dataGridView3.DataSource = lstNG;
+
+            this.CarNumberList.Sort((itemA, itemB) => itemA.CarUsage.Equals(itemB.CarUsage) ? itemB.IsValidNumber.CompareTo(itemA.IsValidNumber) : itemA.CarUsage.CompareTo(itemB.CarUsage));
+            this.dataGridView1.Refresh();
+        }
+    }
+
+    public class RandomAlphaNumeric
+    {
+        public RandomAlphaNumeric(Random rand)
+        {
+            this.Randomizer = rand;
+        }
+
+        public Random Randomizer { get; set; }
+
+        public override string ToString()
+        {
+            if (0 == (this.Randomizer.Next(0,9) % 3))
+            {
+                return ((char)this.Randomizer.Next(65, 90)).ToString();
+            }
+            else
+            {
+                return this.Randomizer.Next(0, 9).ToString();
+            }
+        }
+
+        public static bool IsAlphanumeric(char c)
+        {
+            return char.IsNumber(c) || (65 <= (int)c && (int)c <= 90);
+        }
+    }
+
+    /// <summary>
+    /// 用途車種
+    /// </summary>
+    public enum CarUsageCode
+    {
+        /// <summary>
+        /// 自家用普通乗用
+        /// </summary>
+        JikayouFutuJyouyou1 = 11,
+        /// <summary>
+        /// 自家用普通乗用()
+        /// </summary>
+        JikayouFutuJyouyou2 = 12,
+        /// <summary>
+        /// 自家用小型乗用
+        /// </summary>
+        JikayouKogataJyouyou1 = 13,
+        /// <summary>
+        /// 自家用小型乗用()
+        /// </summary>
+        JikayouKogataJyouyou2 = 14,
+        /// <summary>
+        /// 自家用軽四輪乗用
+        /// </summary>
+        JikayouKeiyonrinJouyou1 = 31,
+        /// <summary>
+        /// 自家用軽四輪乗用()
+        /// </summary>
+        JikayouKeiyonrinJouyou2 = 32,
+        /// <summary>
+        /// 自家用軽四輪貨物
+        /// </summary>
+        JikayouKeiyonrinKamotsu = 33,
+        /// <summary>
+        /// 自家用小型貨物
+        /// </summary>
+        JikayouKogataKamotsu = 49
+    }
+
+    /// <summary>
+    /// 自動車ナンバー
+    /// </summary>
+    public class CarNumber
+    {
+        /// <summary>
+        /// 分類番号最大長
+        /// </summary>
+        public const int MAX_LENGTH_CLASS_NUMBER = 3;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="carUsage">用途車種</param>
+        /// <param name="classNumber">分類番号</param>
+        public CarNumber(CarUsageCode carUsage, string classNumber)
+        {
+            this.CarUsage = carUsage;
+            this.ClassNumber = classNumber;
+        }
+
+        /// <summary>
+        /// 用途車種
+        /// </summary>
+        public CarUsageCode CarUsage { get; set; }
+
+        /// <summary>
+        /// 分類番号
+        /// </summary>
+        public string ClassNumber { get; set; }
+
+        /// <summary>
+        /// 有効なナンバーか判定する
+        /// </summary>
+        public bool IsValidNumber
+        {
+            get
+            {
+                return this.ValidateClassNumber() && true;
+            }
+        }
+
+        /// <summary>
+        /// 分類番号検証
+        /// </summary>
+        /// <returns>有効/無効</returns>
+        private bool ValidateClassNumber()
+        {
+            int classNumber = 0;
+            if (MAX_LENGTH_CLASS_NUMBER.Equals(this.ClassNumber.Length) & !int.TryParse(this.ClassNumber, out classNumber))
+            {
+                switch (this.CarUsage)
+                {
+                    case CarUsageCode.JikayouFutuJyouyou1:
+                    case CarUsageCode.JikayouFutuJyouyou2:
+                        return '3'.Equals(this.ClassNumber[0]) && 
+                                RandomAlphaNumeric.IsAlphanumeric(this.ClassNumber[1]) && RandomAlphaNumeric.IsAlphanumeric(this.ClassNumber[2]);
+
+                    case CarUsageCode.JikayouKogataJyouyou1:
+                    case CarUsageCode.JikayouKogataJyouyou2:
+                    case CarUsageCode.JikayouKeiyonrinJouyou1:
+                    case CarUsageCode.JikayouKeiyonrinJouyou2:
+                        return ('5'.Equals(this.ClassNumber[0]) || '7'.Equals(this.ClassNumber[0])) && 
+                                RandomAlphaNumeric.IsAlphanumeric(this.ClassNumber[1]) && RandomAlphaNumeric.IsAlphanumeric(this.ClassNumber[2]);
+
+                    case CarUsageCode.JikayouKeiyonrinKamotsu:
+                    case CarUsageCode.JikayouKogataKamotsu:
+                        return ('4'.Equals(this.ClassNumber[0]) || '6'.Equals(this.ClassNumber[0]) || '8'.Equals(this.ClassNumber[0])) && 
+                                RandomAlphaNumeric.IsAlphanumeric(this.ClassNumber[1]) && RandomAlphaNumeric.IsAlphanumeric(this.ClassNumber[2]);
+
+                    default:
+                        break;
+                }
+            }
+
+            switch (this.CarUsage)
+            {
+                case CarUsageCode.JikayouFutuJyouyou1:
+                case CarUsageCode.JikayouFutuJyouyou2:
+                    return (3 == classNumber) || (30 <= classNumber && classNumber <= 39) || (300 <= classNumber && classNumber <= 399);
+
+                case CarUsageCode.JikayouKogataJyouyou1:
+                case CarUsageCode.JikayouKogataJyouyou2:
+                    return (5 == classNumber) || (50 <= classNumber && classNumber <= 59) || (500 <= classNumber && classNumber <= 599) ||
+                           (7 == classNumber) || (70 <= classNumber && classNumber <= 79) || (700 <= classNumber && classNumber <= 799);
+
+                case CarUsageCode.JikayouKeiyonrinJouyou1:
+                case CarUsageCode.JikayouKeiyonrinJouyou2:
+                    return (50 <= classNumber && classNumber <= 59) || (500 <= classNumber && classNumber <= 599) ||
+                           (70 <= classNumber && classNumber <= 79) || (700 <= classNumber && classNumber <= 799);
+
+                case CarUsageCode.JikayouKeiyonrinKamotsu:
+                    return (40 <= classNumber && classNumber <= 49) || (400 <= classNumber && classNumber <= 499) ||
+                           (600 <= classNumber && classNumber <= 699) ||
+                           (80 <= classNumber && classNumber <= 89) || (800 <= classNumber && classNumber <= 899);
+
+                case CarUsageCode.JikayouKogataKamotsu:
+                    return (4 == classNumber) || (40 <= classNumber && classNumber <= 49) || (400 <= classNumber && classNumber <= 499) ||
+                           (6 == classNumber) || (60 <= classNumber && classNumber <= 69) || (600 <= classNumber && classNumber <= 699) ||
+                           (8 == classNumber) || (80 <= classNumber && classNumber <= 89) || (800 <= classNumber && classNumber <= 899);
+
+                default:
+                    return true;
+            }
+        }
+    }
+}
